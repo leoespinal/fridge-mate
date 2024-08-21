a4e4a27a7db023b7a40b1bddb501d915438b8fb5
Leo Espinal-Terrero <leo.espinal420@gmail.com>
2024-08-20 22:21:39 -0400

    Initial Commit

---
+++ FridgeMate/FridgeMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FridgeMateApp.swift
+//  FridgeMate
+//
+//  Created by Leo Espinal on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct FridgeMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
